4cc0e446e42fbead06e1f6e556e5104de105e63a
zedonarch <zedonix@proton.me>
2025-10-07 19:58:34 +0530

    nohang

--- install.sh
+++ install.sh
@@ -345,7 +345,7 @@ fi
 if [[ "$extra" == "laptop" ]]; then
   systemctl enable tlp
 fi
-systemctl enable anacron sshd ly
+systemctl enable anacron sshd ly nohang-desktop.service
 systemctl enable NetworkManager NetworkManager-dispatcher
 systemctl mask systemd-rfkill systemd-rfkill.socket
 systemctl disable NetworkManager-wait-online.service
