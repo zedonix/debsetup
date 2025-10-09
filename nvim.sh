7bbb108991c78b3e068b410b3b8a25d34565c6df
zedonarch <zedonix@proton.me>
2025-10-09 10:45:18 +0530

    automated commit

--- nvim.sh
+++ nvim.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # neovim
-sudo apt-get install ninja-build gettext cmake curl build-essential git
 git clone https://github.com/neovim/neovim
 cd neovim
 git checkout stable
-make CMAKE_BUILD_TYPE=RelWithDebInfo build
-cd build
+make CMAKE_BUILD_TYPE=RelWithDebInfo
 cpack -G DEB
 sudo dpkg -i nvim-linux64.deb
